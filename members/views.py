62f8051f623247fc9e67b11ac5d5209d3aee2b16
IliassNazih <iliass.nazih@gmail.com>
2023-03-15 13:17:30 -0400

    Initial commit

---
+++ members/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from .models import Member
+
+# Create your views here.
+def testing(request):
+    mymembers = Member.objects.all().values()
+    template = loader.get_template('myfirst.html')
+    context = {
+        'mymembers': mymembers,
+    }
+    return HttpResponse(template.render(context, request))
+
