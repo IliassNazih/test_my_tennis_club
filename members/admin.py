1dde0d9b9e4675aced9ff74a7af8817653e85431
IliassNazih <iliass.nazih@gmail.com>
2023-03-15 13:48:17 -0400

    Update admin.py

--- members/admin.py
+++ members/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Member
+from django_google_maps import widgets as map_widgets
+from django_google_maps import fields as map_fields
 
 # Register your models here.
 class MemberAdmin(admin.ModelAdmin):
