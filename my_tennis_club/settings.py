69a604a6ede4805c7cef2ba4980d125a065122fa
IliassNazih <iliass.nazih@gmail.com>
2023-03-15 16:12:38 -0400

    Update settings.py

--- my_tennis_club/settings.py
+++ my_tennis_club/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'my_tennis_club.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'masteruser',
+        'PASSWORD': 'Ilia$$2004',
+        'HOST': 'db-app-test.cumcldavzvyo.us-east-2.rds.amazonaws.com',
+        'PORT': '5432'
     }
 }
 
