a98baf6490c07bdddc3a94dc5ead2b97b36cd4fb
IliassNazih <iliass.nazih@gmail.com>
2023-04-03 15:21:21 -0400

    Update settings.py

--- my_tennis_club/settings.py
+++ my_tennis_club/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'my_tennis_club.wsgi.application'
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}'''
+}'''''
 
 DATABASES = {
         'default': {
