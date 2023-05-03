393800aac03bce3d16bbef8ee0a7adaff3a859dc
tudoriordan1 <tudorgk1@gmail.com>
2023-05-03 16:50:16 -0400

    bootstrap my goat

--- my_tennis_club/urls.py
+++ my_tennis_club/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 
 
@@ -9,4 +11,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('base.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
 
