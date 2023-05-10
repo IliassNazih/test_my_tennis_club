211f3d9919019be3d3bd928cc28a4565b4dd9ff4
tudoriordan1 <tudorgk1@gmail.com>
2023-05-10 17:49:41 -0400

    w

--- my_tennis_club/settings.py
+++ my_tennis_club/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'members.apps.MembersConfig',
     'base.apps.BaseConfig',
     'bootstrap5',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -140,3 +142,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
