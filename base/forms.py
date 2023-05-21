b5f0972acbbaa6663a4fec535e5ce15a883228d3
hugogv12 <123586556+hugogv12@users.noreply.github.com>
2023-05-21 14:11:45 -0500

    bug fix

--- base/forms.py
+++ base/forms.py
@@ -5,16 +5,13 @@ from django.contrib.auth.forms import UserCreationForm
 from django import forms
 
 
-
 class ServiceForm(ModelForm):
+    address = forms.CharField(max_length=255, required=True)
+    montant = forms.DecimalField(decimal_places = 2,min_value = 1.00, max_value=1000.00, required= True)
     class Meta:
         model = Service
         fields = '__all__'
-        exclude = ['host', 'participants']
-
-    address = forms.CharField(max_length=255, required=True)
-    montant = forms.DecimalField(decimal_places = 2,min_value = 1.00, max_value=1000.00, required= True)
-    
+        exclude = ['host', 'participants']  
 
 class SignUpForm(UserCreationForm):
     class Meta:
